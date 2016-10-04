f2bbd97289db882baf27deaaedfda6750ad1f5cf
maxsmu <thenear@sina.com>
2016-10-04 17:08:09 +0800

    update mock module

--- mock/index.js
+++ mock/index.js
@@ -2,21 +2,25 @@
  * @Author: maxsmu
  * @Date: 2016-10-04 16:58:23
  * @Last Modified by: maxsmu
- * @Last Modified time: 2016-10-04 16:58:23
+ * @Last Modified time: 2016-10-04 17:07:40
  * @GitHub: https://github.com/maxsmu
 */
 module.exports = {
-	'/index/:id': {
+	'/index': {
 		get: (request, response) => {
-			response.send({ name: '张三', age: 32 });
+			response.send('get');
 		},
-		post: () => console.log(1),
-		put: () => console.log(1),
-		delete: () => console.log(1)
-	},
-	'/menu': {
-		get: (request, response) => {
-			response.send({ name: '张三', age: 32 });
+		post: (request, response) => {
+			response.send('post');
+		},
+		put: (request, response) => {
+			response.send('put');
+		},
+		delete: (request, response) => {
+			response.send('delete');
+		},
+		patch: (request, response) => {
+			response.send('patch');
 		}
 	}
 };
